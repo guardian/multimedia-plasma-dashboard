bdaa101f1ace3ae0f4353cf12847eb304ef7ff62
Andy Gallagher <andy.gallagher@theguardian.com>
2018-01-12 18:00:31 +0000

    basic list-all and query-by-user endpoints

--- app/controllers/DataController.scala
+++ app/controllers/DataController.scala
@@ -8,14 +8,15 @@ import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import com.gu.scanamo._
 import com.gu.scanamo.syntax._
-import models.UnattachedAtom
+import models.{ErrorResponse, UnattachedAtom}
 import play.api.Configuration
 import akka.event.{DiagnosticLoggingAdapter, Logging}
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.auth.{AWSCredentialsProviderChain, EnvironmentVariableCredentialsProvider, InstanceProfileCredentialsProvider}
 import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClientBuilder}
+import com.gu.scanamo.error.DynamoReadError
+import com.gu.scanamo.query.{Between, Query}
 import play.api.Logger
-
 import io.circe._
 import io.circe.generic.auto._
 import io.circe.parser._
@@ -41,26 +42,47 @@ class DataController @Inject()(cc:ControllerComponents,config:Configuration,syst
       .build()
   }
 
-  def all = Action {
-    val tableName:Option[String]=config.getString("UnattachedAtomsTable")
+  val tableName: Option[String] = config.getString("UnattachedAtomsTable")
 
+  def makeResult(result:List[Either[DynamoReadError, UnattachedAtom]]) = {
+    Logger.info(s"$result")
+
+    //https://stackoverflow.com/questions/7230999/how-to-reduce-a-seqeithera-b-to-a-eithera-seqb
+    val processed_result = result collectFirst { case x@Left(_) => x } getOrElse Right(result collect { case Right(x) => x })
+
+    processed_result match {
+      case Left(error) =>
+        val response = ErrorResponse("error", error.toString)
+        InternalServerError(response.asJson.toString)
+      case Right(atomList: List[UnattachedAtom]) =>
+        Ok(atomList.asJson.toString)
+    }
+  }
+
+    def forUser(user:String) = Action { implicit request=>
+      Logger.info(s"unattached atoms table is $tableName")
+
+      val client = getClient
+
+      val table = Table[UnattachedAtom](tableName.get)
+      val userIndex = table.index("userIndex")
+//      val queryParams = request.queryString.map {
+//        case ("user", v) => Some(userIndex.query('userEmail -> v.mkString))
+//        //      case("daterange",v)=>
+//        //        val complete=v.mkString
+//        //        val bounds=complete.split(",")
+//        //        Between('dateCreated, Bounds(Bound(bounds.head),Bound(bounds(1))))
+//        case _ => None
+//      }.filter(_.isDefined)
+      makeResult(Scanamo.exec(client)(userIndex.query('userEmail->user)))
+    }
+
+  def all = Action { implicit request =>
     Logger.info(s"unattached atoms table is $tableName")
 
     val client = getClient
 
     val table = Table[UnattachedAtom](tableName.get)
-
-    val result = Scanamo.exec(client)(table.scan())
-    Logger.info(s"$result")
-
-    //https://stackoverflow.com/questions/7230999/how-to-reduce-a-seqeithera-b-to-a-eithera-seqb
-    val processed_result = result collectFirst { case x@Left(_) => x } getOrElse Right(result collect {case Right(x) => x})
-
-    processed_result match {
-      case Left(error)=>
-        InternalServerError(error.toString)
-      case Right(atomList:List[UnattachedAtom])=>
-        Ok(atomList.asJson.toString)
-    }
+    makeResult(Scanamo.exec(client)(table.scan()))
   }
 }
