7a7247ef854129ffe7b0526b0e85c25761aabe42
Andy Gallagher <andy.gallagher@theguardian.com>
2018-07-27 15:06:00 +0100

    Adding certificates for https comms with AWS

--- .circleci/setup-sbt.sh
+++ .circleci/setup-sbt.sh
@@ -3,5 +3,5 @@
 cd /tmp
 wget https://sbt-downloads.cdnedge.bluemix.net/releases/v1.0.2/sbt-1.0.2.tgz
 cd /usr/share
-tar xvzf /tmp/sbt-1.0.2.tgz
+tar xzf /tmp/sbt-1.0.2.tgz
 rm -rf /tmp/sbt-1.0.2.tgz
